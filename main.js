0af63b8ed60b5af0ce3bcf8688bc18859150440a
MahimaSr05 <72445955+MahimaSr05@users.noreply.github.com>
2020-11-19 17:04:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;  
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
+function setup()
+{
+   video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+}
+
+function gotPoses(results)
+{
+  if(results.length>0)
+  {
+    console.log(results);
+   noseX=results[0].pose.nose.x;
+   noseY=results[0].pose.nose.y;
+    
+  leftWristX=results[0].pose.leftWrist.x;
+  rightWristX=results[0].pose.rightWrist.x;
+  difference=floor(leftWristX-rightWristX);
+  console.log("left Wrist X = "+leftWristX + "right Wrist X ="+rightWristX +"difference="+difference);
+  }
+ }
+
+function draw()
+{
+   background("#3632a8");
+   fill("#ebcc34");
+   stroke("#eb3434");
+  square(noseX,noseY,difference);
+  document.getElementById("square_side").innerHTML="width and height of the square will be "+difference;
+}
